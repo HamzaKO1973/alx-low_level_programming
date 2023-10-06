7df52e0dafc0e1c0d71292a47f6012ce86cd9bfd
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-06 16:46:01 -0700

    ddd

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char ch = 'a';
 
 	while (ch <= 'z')
 	{
-		if (ch != 'q' && ch != 'e')
-		{
-			putchar(ch); /* Print character if it's not 'q' or 'e' */
-		}
+		if (ch == 'e' || ch == 'q'
+			ch++;
+		putchar(ch);
 		ch++;
 	}
 
-	putchar('\n'); /* Print a newline character */
-
+	putchar('\n');
 	return (0);
 }
