1bcc321d75f7e6eae6d74db8cbc91f000caf2457
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-12 12:07:09 -0700

    g

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,20 +1,12 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9, excluding 2 and 4
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void print_most_numbers(void)
+int main(void)
 {
-    int i;
-
-    for (i = 0; i <= 9; i++)
-    {
-        if (i != 2 && i != 4)
-        {
-            _putchar(i + '0');
-        }
-    }
-
-    _putchar('\n');
+    print_most_numbers();
+    return (0);
 }
-
