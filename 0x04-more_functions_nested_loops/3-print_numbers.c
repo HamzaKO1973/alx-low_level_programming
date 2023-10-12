9b62d446a3012784af0d707021b2cc3a74a5c2d4
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-12 12:02:22 -0700

    f

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,12 @@
  */
 void print_numbers(void)
 {
-    char digit = '0';
+	int i = 0;
 
-    while (digit <= '9')
-    {
-        _putchar(digit);
-        digit++;
-    }
-
-    _putchar('\n');
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar(10);
 }
