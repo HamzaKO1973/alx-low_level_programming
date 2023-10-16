7596f00da3347c954ca16f84a42be5d9822b3f62
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-16 11:12:30 -0700

    gg

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,15 @@
 void print_rev(char *s)
 {
 	int length = 0;
+	int i;
 
-	/* Calculate the length of the string*/
+	/* Calculate the length of the string */
 	while (s[length] != '\0')
-	length++;
+		length++;
 
-	/* Print the string in reverse order*/
-	for (int i = length - 1; i >= 0; i--)
-	_putchar(s[i]);
+	/* Print the string in reverse order */
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
 
-	_putchar('\n');
+		_putchar('\n');
 }
