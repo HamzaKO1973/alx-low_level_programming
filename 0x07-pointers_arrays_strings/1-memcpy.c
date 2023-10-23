b06fdc6cfbd40223a6732ad8a2c3b745f3f8e5dd
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-23 14:19:11 -0700

    gg

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * custom_memcpy - Copies memory area.
+ * @dest: A pointer to the destination memory area.
+ * @src: A pointer to the source memory area.
+ * @n: The number of bytes to be copied.
+ *
+ * Return: A pointer to the destination memory area (dest).
+ */
+char *custom_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *new_variable = dest;
+
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+return (new_variable);
+}
