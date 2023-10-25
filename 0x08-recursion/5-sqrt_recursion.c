47bfb73a7494330501cdc5024ea6e483ef0034ec
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-25 15:38:05 -0700

    gg

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,3 +11,12 @@ int _sqrt_recursion(int n)
 {
 return (square(n, 1));
 }
+int square(int n, int val)
+{
+if (val * val == n)
+	return (val);
+else if (val * val < n)
+	return (aquare(n, val + 1));
+else
+	return (-1);
+}
