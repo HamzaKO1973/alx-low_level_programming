f61d96f9d245bab699c74c2ec60335e3d7f5c625
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-25 16:02:36 -0700

    gg

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,24 +8,24 @@
  */
 
 int check_prime(int n, int f);
-int is_prime_number (int n)
+int is_prime_number(int n)
 {
-return (check_prime (n, 2));
+return (check_prime(n, 2));
 }
 
 /**
  * check_prime - check all number < n if they can divise it
  * @n: int
- * #f: int
+ * @f: int
  * Return: int
  */
 
-int check_prime (int n, int f)
+int check_prime(int n, int f)
 {
 if (f >= n && n > 1)
 	return (1);
 else if (n % f == 0 || n <= 1)
 	return (0);
 else
-	return (check_prime (n, f + 1));
+	return (check_prime(n, f + 1));
 }
