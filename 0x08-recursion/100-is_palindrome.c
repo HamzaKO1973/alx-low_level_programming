6274fd837656351e1d1e70c59d8a5d431e509081
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-26 14:14:35 -0700

    gg

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,12 +8,12 @@
  */
 int is_palindrome(char *s)
 {
-	int i = 0;
-	unsigned long int j = strlen(s) - 1;
+	int a = 0;
+	unsigned long int b = strlen(s) - 1;
 
 	if (s == 0)
 		return (1);
-	return (test_palindrome(s, i, j));
+	return (test_palindrome(s, a, b));
 }
 /**
  * test_palindrome - test for palindrome string.
