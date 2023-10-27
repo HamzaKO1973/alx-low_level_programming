b771154427d214db1e0533db409d5b9230c32378
HamzaKO1973 <hamouzmiloudi@gmail.com>
2023-10-27 12:46:36 -0700

    gg

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,7 +6,7 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
 	int i =0;
 
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the minimum number of coins
+ * @argc: argc
+ * @argv: argv 
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+	int i, lea = 0, mon = atoi(argv[1]);
+	int cents[] = {25, 10, 5, 2, 1};
+
+	for (i = 0; i < 5; i++)
+	{
+
+		if (mon >= cents[i])
+		{
+			lea += mon / cents[i];
+			mon = mon % cents[i];
+			if (money % cents[i] == 0)
+			{
+				break;
+			}
+		}
+	}
+
+	printf("%d\n", lea);
+	}
+	else
+	{
+		print("Error\n"0;
+		return (1);
+	}
+	return (0);
+}
